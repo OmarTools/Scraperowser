78345e7d69d6d767a8c4423942f5ab298d478cf3
Masked programmer <Masked programmer@HUAWEI>
2024-06-25 22:20:54 +0300

    Add project files.

---
+++ urlBrowser/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace urlBrowser
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(16, 15);
+            this.txtUrl.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(532, 22);
+            this.txtUrl.TabIndex = 0;
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(557, 12);
+            this.btnLoad.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(100, 28);
+            this.btnLoad.TabIndex = 1;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(672, 543);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.txtUrl);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Scraperowser";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.Button btnLoad;
+    }
+}
---
+++ urlBrowser/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
+
+namespace urlBrowser
+{
+    public partial class Form1 : Form
+    {
+        private ChromiumWebBrowser browser;
+        private string urlsFilePath = "C:\\browsertemp\\urls.txt";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CefSettings settings = new CefSettings();
+            Cef.Initialize(settings);
+
+            browser = new ChromiumWebBrowser();
+            browser.Dock = DockStyle.Fill;
+            browser.FrameLoadEnd += Browser_FrameLoadEnd;
+            Controls.Add(browser);
+        }
+
+        private void Browser_FrameLoadEnd(object sender, FrameLoadEndEventArgs e)
+        {
+            browser.GetBrowser().MainFrame.GetSourceAsync().ContinueWith(task =>
+            {
+                var htmlContent = task.Result;
+
+                var urls = ExtractResourceUrls(htmlContent);
+                SaveUrlsToFile(urls);
+            });
+        }
+
+        private string[] ExtractResourceUrls(string htmlContent)
+        {
+            var regexPattern = @"(http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+)";
+
+            var matches = System.Text.RegularExpressions.Regex.Matches(htmlContent, regexPattern);
+
+            var urls = new string[matches.Count];
+            for (int i = 0; i < matches.Count; i++)
+            {
+                urls[i] = matches[i].Groups[1].Value;
+            }
+
+            return urls;
+        }
+
+        private void SaveUrlsToFile(string[] urls)
+        {
+            using (var writer = new StreamWriter(urlsFilePath))
+            {
+                foreach (var url in urls)
+                {
+                    writer.WriteLine(url);
+                }
+            }
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            string url = txtUrl.Text;
+
+            if (!string.IsNullOrEmpty(url))
+            {
+                browser.Load(url);
+            }
+        }
+    }
+}
